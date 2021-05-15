7f2576bbe62b56825573f1415a1bed52758beaec
LW <elnewatts@gmail.com>
2021-05-15 12:17:25 +0000

    transaction pool added

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -26,7 +26,7 @@ class Blockchain:
         try:
          Blockchain.is_valid_chain(chain)
         except Exception as e:
-            raise Exception(f'Cannot rreplace. The incoming chain is invalid: {e}')
+            raise Exception(f'Cannot replace. The incoming chain is invalid: {e}')
 
         self.chain = chain
 
@@ -35,6 +35,17 @@ class Blockchain:
         Serialise the blockchain into a list of blocks.
         """
         return list(map(lambda block: block.to_json(), self.chain))
+
+    @staticmethod
+    def from_json(chain_json):
+        """
+        Deserialize a list of serialized blocks into a Blockchain instance.
+        The result will contain a chain list of Block instances.
+        """ 
+        blockchain = Blockchain()
+        blockchain.chain = list(map(lambda block_json: Block.from_json(block_json), chain_json))
+
+        return blockchain
 
     @staticmethod
     def is_valid_chain(chain):
